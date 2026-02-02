b122e62e3e1c8298391f1c0e0d87e9517efeb59b
Heloi Neto <hey@heloineto.com>
2026-02-02 13:47:25 -0300

    corrigir problemas e adicionar estilos css

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
-      include: '**/*.{jsx,js}',
+      include: "**/*.{jsx,js}",
     }),
   ],
   esbuild: {
-    loader: 'jsx',
+    loader: "jsx",
     include: /src\/.*\.jsx?$/,
     exclude: [],
   },
   optimizeDeps: {
     esbuildOptions: {
       loader: {
-        '.js': 'jsx',
+        ".js": "jsx",
       },
     },
   },
-})
+});
